8e00abf94d6af1dc3ada1a6c503ca8b297aa6577
Paulo Campos <pacampos@gmail.com>
2017-10-01 21:52:20 -0300

    Modificações na tela de professor

--- app/src/main/java/com/a3ti/school/MainActivity.java
+++ app/src/main/java/com/a3ti/school/MainActivity.java
@@ -5,10 +5,12 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     private Button btLogar;
+    private TextView btEsqueciSenha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +18,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btLogar = (Button) findViewById(R.id.btLogar);
+        btEsqueciSenha = (TextView) findViewById(R.id.btEsqueciSenha);
+
+        btEsqueciSenha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(MainActivity.this, EnviaSenhaActivity.class));
+            }
+        });
 
         btLogar.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/a3ti/school/ProfessorPrincipalActivity.java
+++ app/src/main/java/com/a3ti/school/ProfessorPrincipalActivity.java
@@ -3,25 +3,58 @@ package com.a3ti.school;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.view.menu.MenuView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 
 public class ProfessorPrincipalActivity extends AppCompatActivity {
 
-    private Button btSair;
+    private ImageView btSair;
+    private ImageView btChamada;
+    private ImageView btRelatorio;
+    private ImageView btNotas;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_professor_principal);
 
-        btSair = (Button) findViewById(R.id.btSair);
+        btSair = (ImageView) findViewById(R.id.btSair);
+        btChamada = (ImageView) findViewById(R.id.btChamda);
+        btRelatorio = (ImageView) findViewById(R.id.btRelatorio);
+        btNotas = (ImageView) findViewById(R.id.btNotas);
 
-       btSair.setOnClickListener(new View.OnClickListener() {
-           @Override
-           public void onClick(View view) {
-               startActivity(new Intent(ProfessorPrincipalActivity.this, MainActivity.class));
-           }
-       });
+
+        btChamada.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfessorPrincipalActivity.this, ChamadaActivity.class));
+            }
+        });
+
+        btNotas.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfessorPrincipalActivity.this, IncluirNotasActivity.class));
+            }
+        });
+
+
+        btRelatorio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfessorPrincipalActivity.this, RElAlunosActivity.class));
+            }
+        });
+
+        btSair.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ProfessorPrincipalActivity.this, MainActivity.class));
+            }
+        });
+
+
     }
 }
